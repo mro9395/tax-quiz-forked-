0ee9279bd61930bf47bab66e23e7714e1b4a1373
Mauricio Rada <mro9395@gmail.com>
2024-01-06 02:07:19 +0000

    feat: Update styling for pricing page, fix broken link in Logo component and add Pricing page component and router configuration

---
+++ src/router/router.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Dashboard from "@/views/Dashboard";
+Vue.use(Router);
+
+const routes = [
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    title: "Dashboard",
+    component: Dashboard,
+  },
+  {
+    path: "*",
+    redirect: { name: "dashboard" },
+  },
+];
+
+export default new Router({
+  routes,
+});
